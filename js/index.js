b4b18ffac6f76efaffbc89be16679bce2c1b869d
Sohn0123 <ssmssm0123@gmail.com>
2016-06-27 11:49:38 -1000

    YEY

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var arr = ['red', 'orange',  'yellow' 'green', 'blue',];
+var count = 0;
+
+var cycle = function(array/*perameter*/){
+
+console.log(array[count]);
+count[++]; //or count += 1; or count = count +
+};
+
+var list = function
