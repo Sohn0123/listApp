4edfe711c3b17a421be81c2cad1b46fd3ed6191e
Sohn0123 <ssmssm0123@gmail.com>
2016-06-28 12:00:05 -1000

    YeY

--- js/index.js
+++ js/index.js
@@ -1,10 +1,32 @@
-var arr = ['red', 'orange',  'yellow' 'green', 'blue',];
+var arr = [
+	 "red",
+	 "orange", 
+	 "yellow",
+	 "green", 
+	 "blue",
+	 "indigo",
+	 "violet",
+];
 var count = 0;
 
-var cycle = function(array/*perameter*/){
+var cycle = function(array) {
+	console.log(array[count]);
 
-console.log(array[count]);
-count[++]; //or count += 1; or count = count +
+	if (count == array.length-1) {
+		count=0;
+	} else {
+		count++;
+	};
 };
+var display = function(){
+	document.body.style.color = colors[count];
+	cycle(colors);
 
-var list = function
+};
+var listAll = function(array) {
+	for (var i = 0; i < array.length; i++) {
+		console.log(i)
+		document.getElementById('colorSpace') innerHTML += "<li>" + array[i] + "</li>";
+	}
+}
+listAll(colors)
